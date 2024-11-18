3351d6849664103494b33e72c173c1587c45e6b8
jaejin <jaejin0109@gmail.com>
2024-11-18 01:44:23 -0600

    added a written description on the concept of entropy

--- jal_am.py
+++ jal_am.py
@@ -8,27 +8,37 @@ class NeuralNetwork(nn.Module):
         super().__init__()
         self.network = nn.Sequential(
             nn.Linear(input_dim, hidden_dim, dtype=float),
-            # nn.Dropout(0.5),
+            nn.Dropout(0.5),
             nn.ReLU(),
             nn.Linear(hidden_dim, hidden_dim, dtype=float),
-            # nn.Dropout(0.5),
+            nn.Dropout(0.5),
             nn.ReLU(),
             nn.Linear(hidden_dim, output_dim, dtype=float),
-            # nn.Dropout(0.5),
+            nn.Dropout(0.5),
             nn.ReLU()
         )
 
     def forward(self, x):
         x = self.network(x)
-        print(x)
-        # output = F.log_softmax(x, dim=3)
-        # print("output", output)
+        output = F.softmax(x, dim=0)
+        print("output", output)
         return x
 
 '''
 agents learn models of other agents j conditioned on states s: estimated pi_{-i}(a_{-i} | s)
 agents learn value functions conditioned on the joint action: Q_i(s, a)
 using the value function and agent models, agent i can compute its expected action values under the current models of other agents: AV_i(s, a_i) = SUM_{a_{-i} in A_{-i}}Q_i(s, <a_i, a_{-i}>) PI_{j in I\{i}} estimated pi_j (a_j | s)
+minimize cross-entropy loss between the predicted policy and the observed actions of agent j
+'''
+
+'''
+NOTE ON ENTROPY
+entropy in thermodynamics : degree of disorder OR randomness in a system
+interpreted using probability -> probablity of each energy configuration; higher entropy is statistically more likely
+
+entropy in information theory : expected value of surprise, where expected value is x * p(x) + ... + y + p(y), surprise = log_n(1/p(x)), where n is the number of possible output
+entropy = SUM(p(x) * log_n(1/p(x))) = -SUM(p(x) * log(p(x)))
+by Claude Shannon
 '''
 
 # Joint-Action Learning with Deep Agent Modeling
@@ -42,7 +52,7 @@ class JAL_AM:
         self.agent_model = NeuralNetwork(observation_dim, action_dim)
 
         # train agent model using the obseration history
-        self.observation_history = np.zeros()
+        # self.observation_history = np.zeros()
 
         self.loss_fn = nn.CrossEntropyLoss()
         
@@ -54,7 +64,6 @@ class JAL_AM:
     def learn(self, observation, joint_action):
         pass
 
-        # minimize cross-entropy loss between the predicted policy and the observed actions of agent j
     def train_agent_model(self):
         pass
 
